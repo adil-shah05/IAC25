379615d3b88405d4e0a05979c9568432dca85bc2
adil <adilanakkot@gmail.com>
2025-10-16 23:13:22 +0100

    Task 1 Extension Done

--- labs/Lab1-Counter/task1/counter_tb.cpp
+++ labs/Lab1-Counter/task1/counter_tb.cpp
@@ -2,37 +2,63 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
-int main(int argc, char **argv, char **env) {
+int main(int argc, char **argv, char **env)
+{
     int i;
     int clk;
 
     Verilated::commandArgs(argc, argv);
     // init top verilog instance
-    Vcounter* top = new Vcounter;
+    Vcounter *top = new Vcounter;
     // init trace dump
     Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp = new VerilatedVcdC;
-    top->trace (tfp, 99);
-    tfp->open ("counter.vcd");
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("counter.vcd");
 
     // initialize simulation inputs
     top->clk = 1;
     top->rst = 1;
     top->en = 0;
 
+    int stopclk = 0;      // counter for stopping cycles
+    bool stopped = false; // flag to ensure we only stop once
+
     // run simulation for many clock cycles
-    for (i=0; i<300; i++) {
+    for (i = 0; i < 300; i++)
+    {
 
         // dump variables into VCD file and toggle clock
-        for (clk=0; clk<2; clk++) {
-            tfp->dump (2*i+clk);        // unit is in ps!!!
+        for (clk = 0; clk < 2; clk++)
+        {
+            tfp->dump(2 * i + clk); // unit is in ps!!!
             top->clk = !top->clk;
-            top->eval ();
+            top->eval();
         }
 
-        top->rst = (i <2) | (i == 15);
-        top->en = (i>4);
-        if (Verilated::gotFinish())  exit(0);
+        // Reset only at the beginning
+        top->rst = (i < 2);
+
+        // Check if counter reached 0x9 for the first time
+        if (top->count == 0x9 && !stopped)
+        {
+            stopclk = 3;    // stop for 3 cycles
+            stopped = true; // mark that we've stopped
+        }
+
+        // Handle enable signal
+        if (stopclk > 0)
+        {
+            top->en = 0; // disable counting during pause
+            stopclk--;
+        }
+        else
+        {
+            top->en = (i > 4); // enable after initial reset
+        }
+
+        if (Verilated::gotFinish())
+            exit(0);
     }
     tfp->close();
     exit(0);
