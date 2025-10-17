24da3e529507162862e37d7ed48f8b85f434ac4f
SAMOS-76 <sosebeyo@gmail.com>
2025-10-17 09:09:41 +0100

    Task 2 Lab 1

---
+++ labs/Lab1-Counter/task2/counter_tb.cpp
@@ -0,0 +1,50 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vcounter* top = new Vcounter;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab1: Counter");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation for many clock cycles
+    for (i=0; i<300; i++) {
+
+        // dump variables into VCD file and toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump (2*i+clk);        // unit is in ps!!!
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        vbdHex(4, (int(top->count) >> 16) & 0XF);
+        vbdHex(3, (int(top->count) >> 8) & 0XF);
+        vbdHex(2, (int(top->count) >> 4) & 0XF);
+        vbdHex(1, int(top->count) & 0XF);
+        vbdCycle(i+1);
+
+        top->rst = (i <2) | (i == 15);
+        top->en = (i>4);
+        if (Verilated::gotFinish())  exit(0);
+    }
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
