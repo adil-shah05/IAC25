7bebb3fb9f5bb50093d719fb23673f82d5edabc0
SAMOS-76 <sosebeyo@gmail.com>
2025-11-02 17:54:39 +0000

    Lab3 test yourself

--- labs/Lab3-FSM-main/Lab3-FSM-main/task3/doit.sh
+++ labs/Lab3-FSM-main/Lab3-FSM-main/task3/doit.sh
@@ -1,12 +1,25 @@
+# #!/bin/sh
+
+# ~/Documents/iac/lab0-devtools/tools/attach_usb.sh
+
+# rm -rf obj_dir
+# rm -f clktick.vcd
+
+# verilator -Wall --cc --trace clktick.sv --exe clktick_tb.cpp
+ 
+# make -j -C obj_dir/ -f Vclktick.mk Vclktick
+
+# obj_dir/Vclktick
+
 #!/bin/sh
 
 ~/Documents/iac/lab0-devtools/tools/attach_usb.sh
 
 rm -rf obj_dir
-rm -f clktick.vcd
+rm -f f1.vcd
 
-verilator -Wall --cc --trace clktick.sv --exe clktick_tb.cpp
+verilator -Wall --cc --trace f1.sv --exe f1_fsm_tb.cpp
  
-make -j -C obj_dir/ -f Vclktick.mk Vclktick
+make -j -C obj_dir/ -f Vf1.mk Vf1
 
-obj_dir/Vclktick
+obj_dir/Vf1
